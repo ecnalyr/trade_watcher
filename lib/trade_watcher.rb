773911202f33ebd95b7f9040aa29b8f3dc60a07e
Jason Rylance <ecnalyr@gmail.com>
2013-04-15 10:35:59 -0500

    Added the ability to check if a stock has reached a target value over time.

--- lib/trade_watcher.rb
+++ lib/trade_watcher.rb
@@ -9,4 +9,22 @@ module TradeWatcher
   def self.is_stock_at_or_above_value(symbol, value)
     last_trade(symbol).to_f >= value
   end
+
+  def self.check_stock_every_x_seconds_for_value(symbol, seconds, value)
+    t1 = Thread.new{(self.checker(symbol, seconds, value))} 
+  end
+
+  private
+      def self.checker(symbol, seconds, value)
+        stop_time = Time.now + 3 # an hour from Time.now
+        until self.is_stock_at_or_above_value(symbol, value) || Time.now >= stop_time
+          pp "#{Time.now} #{symbol} has not met your target of #{value}."
+          sleep(seconds)
+        end
+        target_value_reached(symbol, value)
+      end
+
+      def target_value_reached(symbol, value)
+        pp "#{symbol} has met or surpassed your target of  #{value}"
+      end
 end
--- spec/trade_watcher_spec.rb
+++ spec/trade_watcher_spec.rb
@@ -13,5 +13,18 @@ describe  "TradeWatcher" do
     end
   end
 
+  it "checks stock value multiple times while stock is not at or above the target value" do
+    TradeWatcher.stub!(:last_trade).and_return(200)
+    TradeWatcher.should_receive(:is_stock_at_or_above_value).at_least(2).times
+    TradeWatcher.check_stock_every_x_seconds_for_value(:AAPL, 1, 400)
+    sleep(2)
+  end
+
+  it "triggers target_value_reahed when the stock has met or surpassed the target value" do
+    TradeWatcher.stub!(:last_trade).and_return(200)
+    TradeWatcher.should_receive(:target_value_reached).exactly(1).times
+    TradeWatcher.check_stock_every_x_seconds_for_value(:AAPL, 1, 100)
+    sleep(1)
+  end
 
 end
